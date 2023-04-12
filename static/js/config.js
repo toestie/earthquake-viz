d28f1a52b60d7e66c86a93d0ccff59d76e78c382
Nathan Wong <nathan.christopher.wong@gmail.com>
2023-04-12 16:39:00 -0500

    leaflet map added

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-API_KEY = "pk.eyJ1IjoidG9lc3RpZSIsImEiOiJjazJycnF2ZWYwY29pM2RudGl5eXB0MHMwIn0.4h0NRWJuwefRJ-4erWlFZw"
+API_KEY = "pk.eyJ1IjoidG9lc3RpZSIsImEiOiJjbGdlNzl6bHcwcm1jM2VyeTZkc24wOTc3In0.oUpf7nA0bC9_G7mq_78F-A"
